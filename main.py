5f3efbede8db98a91ac013b51a4f00a2bd32d6e2
noahansen <nphansen@wisc.edu>
2020-04-23 22:47:22 +0000

    Modified index and added routes to pages

--- main.py
+++ main.py
@@ -6,8 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("plots.html")
+    return render_template("index.html")
     #or jsut return graph.toHtml
+
+@app.route('/data')
+def data():
+    return render_template("data.html")
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
+
+@app.route('/acks')
+def acks():
+    return render_template("acks.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0") 
